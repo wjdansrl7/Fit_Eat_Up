e9e22b5eae5f62ea339560a1f87e99bd1030f694
yerim03 <na90303@naver.com>
2022-04-28 20:35:30 +0900

    version 1.4.1

--- Fit_Eat_Up/src/contexts/index.js
+++
@@ -1,7 +0,0 @@
-// context API 관리
-// 추후 필요하면 추가
-
-import { ProgressContext, ProgressProvider } from "./Progress";
-import { UserContext, UserProvider } from "./User";
-
-export { ProgressContext, ProgressProvider, UserContext, UserProvider };
