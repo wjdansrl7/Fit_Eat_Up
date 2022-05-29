adb0d9e270f855cdb79cb969a7de71cef0bd9dbb
kimjm9911 <kimjm9911naver.com>
2022-05-29 23:48:25 +0900

    urls 추가

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from friend import views
+from user import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls') ),
     path('post/', views.post_view),
+    path('review/', views.review_view),
 ]
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from user.models import Client, Review
 
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
