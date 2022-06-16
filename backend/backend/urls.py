e23976286b933ea5bd2fe150020c996a5032c4f6
moongi <c880910@naver.com>
2022-06-16 14:06:32 +0900

    User 모델 생성

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,24 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from friend import views
-from user import views as v2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls') ),
-    path('post/', views.post_view),
-    path('review/', v2.review_view),
+    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include("accounts.urls")),
+    path('friend/', include("friend.urls")),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+
+
--- backend/user/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from user.models import Review
-# Create your views here.
-
-def review_view(request):
-    reviews = Review.objects.all()
-    return render(request)
